37acbce6029f0074847929772ce93a1ce123fc2d
Cedric Lang-Roth <dnj855@yahoo.com>
2025-03-05 12:12:54 +0100

    Fix the strapi connexion

---
+++ pages/blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <div v-for="article in articles.data" :key="article.id">
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: articles } = await useFetch('http://localhost:1337/api/articles');
+</script>
+
+<style scoped>
+
+</style>
